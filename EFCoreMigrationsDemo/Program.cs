861febc0d4f153c284c80de770ea7a967002b252
Marcin Węglowski <mawg@softwaremind.com>
2023-09-19 11:38:48 +0200

    Add package Microsoft.EntityFramework.Design

--- EFCoreMigrationsDemo/Program.cs
+++ EFCoreMigrationsDemo/Program.cs
@@ -3,7 +3,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System.IO;
 
 var builder = new HostBuilder()
     .ConfigureServices((hostContext, services) => {
